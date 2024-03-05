4fb37ab5af8d48900898ca8ce8ee938f051d4e8b
Luke H <83400837+JoelSnf@users.noreply.github.com>
2024-03-05 02:52:35 +0000

    Making drones which are too close avoid eachother (#2)
    * updatedCentralControl
    
    * centralControl now detects when drones are too close together and sends them into a new 'panic' mode.
    They are also given a velocity to fly by centralControl.
    
    Also added restrictions on the dimensions and NaN allowance in the properties of Drone, and PhysicalObject.
    
    main has been modified to run centralControl.DronesAvoidance()

--- CentralControl.m
+++ CentralControl.m
@@ -8,59 +8,94 @@ classdef CentralControl
             
         % end
 
-        function [distancesMatrix] = CalculateDistancesMatrix(~, dronePositions) 
-            % Returns a symmetric matrix which is NxN: N is the number of drones. 
-            % Top left is distance between drone 1 and drone 1. 
-            % Bottom left is distance between drone 4 and drone 1.
-            distancesMatrix = NaN(size(dronePositions,2));
-            
-            for i = 1 : size(dronePositions,2)
-                drone1Position = dronePositions(:,i);
+% ========= this entire function is not actually helpful so i've commented it just incase its handy to copy from =========
 
-                for j = 1 : size(dronePositions,2)
-                    drone2Position = dronePositions(:,j);
+        % function [distancesMatrix] = CalculateDistancesMatrix(~, dronePositions) 
+        %     % Returns a symmetric matrix which is NxN: N is the number of drones. 
+        %     % Top left is distance between drone 1 and drone 1. 
+        %     % Bottom left is distance between drone N and drone 1.
+        %     distancesMatrix = NaN(size(dronePositions,2));
+        % 
+        %     for i = 1 : size(dronePositions,2)
+        %         droneiPosition = dronePositions(:,i);
+        % 
+        %         for j = 1 : size(dronePositions,2)
+        %             dronejPosition = dronePositions(:,j);
+        % 
+        %             droneiIsDronej = i == j;
+        %             if droneiIsDronej % if the 2 drones are the same, we can leave the distance as NaN
+        %                 continue
+        %             end
+        % 
+        %             vectorBetweenDrones = dronejPosition - droneiPosition;
+        %             distanceBetweenDrones = norm(vectorBetweenDrones);
+        % 
+        %             distancesMatrix(i,j) = distanceBetweenDrones;
+        %         end
+        %     end
+        % end
 
-                    drone1IsDrone2 = i == j;
-                    if drone1IsDrone2 % if the 2 drones are the same, we can leave the distance as NaN
-                        continue
-                    end
+        function vectorToEscape = FindEscapeDirection(obj,drone,otherDronePositions) 
+        % find the direction that a given drone should fly away if it's too
+        % close to another drone(s).
 
-                    vectorBetweenDrones = drone2Position - drone1Position;
-                    distanceBetweenDrones = norm(vectorBetweenDrones);
+            posOfdronesTooClose =  obj.FindNearbyDrones(drone,otherDronePositions);
 
-                    distancesMatrix(i,j) = distanceBetweenDrones;
-                end
-            end
-        end
-
-        function DroneDangerClusters(obj,dronePositions) 
-            % link drones into groups when they are too close to eachother. 
-            % (Should usually happen with groups of 2!)
-            distancesMatrix = CalculateDistancesMatrix(obj, dronePositions);
-
-            allDronesSafe = min(distancesMatrix,[],"all") > Drone.safetyAreaRadius;
-
-            if allDronesSafe
+            if isempty(posOfdronesTooClose)
+                vectorToEscape = [0,0]';
                 return
             end
 
-            for i = 1 : size(distancesMatrix)
-                for j = 1 : size(distancesMatrix)
-                    distanceBetweenDroneiandDronej = distancesMatrix(i,j);
-                   
-                    if isnan(distanceBetweenDroneiandDronej)
-                        continue
+            centerOfMass = sum(posOfdronesTooClose, 2) / size(posOfdronesTooClose,2);          
 
-                    elseif distanceBetweenDroneiandDronej < Drone.safetyAreaRadius
+            vectorToEscape = drone.position - centerOfMass;
+            
+            if vectorToEscape == 0
+                vectorToEscape = rand(2,1);
+            end
 
-                        % TODO: link dronei and dronej together
-                        
-                    end
+            vectorToEscape = vectorToEscape  / norm(vectorToEscape);
+            return
 
-                end
+        end
+
+        function MakeDroneRunFromNeighbors(obj,drone,otherDronePositions)
+            % signal to a drone that it must flee , and in which direction
+
+            vectorToEscape = obj.FindEscapeDirection(drone,otherDronePositions);
+
+            droneIsInDanger = all( vectorToEscape ~= 0 ); 
+            drone.panicMode = droneIsInDanger;                   
+            drone.escapeDirection = vectorToEscape;
+
+        end
+
+        function DronesAvoidance(obj,drones)
+            droneQuantity = length(drones);
+
+            dronePositions = zeros(2,droneQuantity);
+            for j = 1:droneQuantity
+                dronePositions(:,j) = drones{j}.position;
+            end
+
+            for z = 1:droneQuantity
+                dronePositionsWithoutZ = dronePositions;
+                dronePositionsWithoutZ(:,z) = [];
+                obj.MakeDroneRunFromNeighbors(drones{z},dronePositionsWithoutZ);    
             end
 
         end
 
+        function nearbyDrones = FindNearbyDrones(~,drone,otherDronePositions)
+            nearbyDrones = [];
+            for i = 1 : size(otherDronePositions,2)
+                droneiPosition = otherDronePositions(:,i);        
+                if norm(drone.position - droneiPosition) < Drone.safetyAreaRadius                    
+                    nearbyDrones = [nearbyDrones , droneiPosition]; %#ok<AGROW>
+                end    
+            end
+        end
+
     end
+
 end
--- PhysicalObject.m
+++ PhysicalObject.m
@@ -1,9 +1,9 @@
 classdef PhysicalObject < handle
 
     properties
-        position = [20,100]'; % starting location, in metres
-        velocity = [0,0]'; % m/s.            Consumer drones go ~20 m/s max
-        acceleration = [0,0]'; % m/s^2
+        position (2,1) {mustBeNonNan}  = [20,100]'; % starting location, in metres
+        velocity (2,1) {mustBeNonNan} = [0,0]'; % m/s.            Consumer drones go ~20 m/s max
+        acceleration (2,1) {mustBeNonNan} = [0,0]'; % m/s^2
     end
 
     properties (Constant)
