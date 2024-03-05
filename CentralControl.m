edbdc1647c9c108af6f1ba1a0715af66d46de690
Luke H <luke.hayes55@yahoo.co.uk>
2024-03-05 00:15:53 +0000

    updatedCentralControl

--- CentralControl.m
+++ CentralControl.m
@@ -7,60 +7,64 @@ classdef CentralControl
         % function obj = CentralControl(inputArg1)
             
         % end
+        
+% ========= this entire function is not actually helpful so i've commented it just incase =========
 
-        function [distancesMatrix] = CalculateDistancesMatrix(~, dronePositions) 
-            % Returns a symmetric matrix which is NxN: N is the number of drones. 
-            % Top left is distance between drone 1 and drone 1. 
-            % Bottom left is distance between drone 4 and drone 1.
-            distancesMatrix = NaN(size(dronePositions,2));
-            
+        % function [distancesMatrix] = CalculateDistancesMatrix(~, dronePositions) 
+        %     % Returns a symmetric matrix which is NxN: N is the number of drones. 
+        %     % Top left is distance between drone 1 and drone 1. 
+        %     % Bottom left is distance between drone N and drone 1.
+        %     distancesMatrix = NaN(size(dronePositions,2));
+        % 
+        %     for i = 1 : size(dronePositions,2)
+        %         droneiPosition = dronePositions(:,i);
+        % 
+        %         for j = 1 : size(dronePositions,2)
+        %             dronejPosition = dronePositions(:,j);
+        % 
+        %             droneiIsDronej = i == j;
+        %             if droneiIsDronej % if the 2 drones are the same, we can leave the distance as NaN
+        %                 continue
+        %             end
+        % 
+        %             vectorBetweenDrones = dronejPosition - droneiPosition;
+        %             distanceBetweenDrones = norm(vectorBetweenDrones);
+        % 
+        %             distancesMatrix(i,j) = distanceBetweenDrones;
+        %         end
+        %     end
+        % end
+
+        function vectorToEscape = findEscapeDirection(obj,drone,dronePositions) 
+        % find the direction that a given drone should fly away if it's too
+        % close to another drone(s).
+
+            positionsOfdronesWhichAreTooClose = [];
+
             for i = 1 : size(dronePositions,2)
-                drone1Position = dronePositions(:,i);
-
-                for j = 1 : size(dronePositions,2)
-                    drone2Position = dronePositions(:,j);
-
-                    drone1IsDrone2 = i == j;
-                    if drone1IsDrone2 % if the 2 drones are the same, we can leave the distance as NaN
-                        continue
-                    end
-
-                    vectorBetweenDrones = drone2Position - drone1Position;
-                    distanceBetweenDrones = norm(vectorBetweenDrones);
-
-                    distancesMatrix(i,j) = distanceBetweenDrones;
-                end
+                droneiPosition = dronePositions(:,i);
+            
+                if drone.position == droneiPosition
+                    continue
+                
+                elseif norm(drone.position - droneiPosition) < Drone.safetyAreaRadius                    
+                    positionsOfdronesWhichAreTooClose = [positionsOfdronesWhichAreTooClose , droneiPosition];
+                end    
             end
-        end
 
-        function DroneDangerClusters(obj,dronePositions) 
-            % link drones into groups when they are too close to eachother. 
-            % (Should usually happen with groups of 2!)
-            distancesMatrix = CalculateDistancesMatrix(obj, dronePositions);
-
-            allDronesSafe = min(distancesMatrix,[],"all") > Drone.safetyAreaRadius;
-
-            if allDronesSafe
+            ourDroneIsSafe = size(positionsOfdronesWhichAreTooClose,2) == 0;
+            if ourDroneIsSafe
+                vectorToEscape = NaN
                 return
             end
 
-            for i = 1 : size(distancesMatrix)
-                for j = 1 : size(distancesMatrix)
-                    distanceBetweenDroneiandDronej = distancesMatrix(i,j);
-                   
-                    if isnan(distanceBetweenDroneiandDronej)
-                        continue
+            centerOfMass = sum(positionsOfdronesWhichAreTooClose, 2) / size(positionsOfdronesWhichAreTooClose,2);
 
-                    elseif distanceBetweenDroneiandDronej < Drone.safetyAreaRadius
-
-                        % TODO: link dronei and dronej together
-                        
-                    end
-
-                end
-            end
+            vectorToEscape = drone.position - centerOfMass;
+            return
 
         end
+        
 
     end
 end
