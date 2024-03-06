08263fc9192cea05600c7c6f35d9e0286b109109
SwolfyTDP <SwolfyTDP@gmail.com>
2024-03-06 18:25:42 +0000

    Checked for 0's

--- Path Creator ProgramOUTDATED.py
+++ Path Creator ProgramOUTDATED.py
@@ -1,20 +1,36 @@
 import math
 #This can be used to visualise the thing later however I have completly forgot how to get matplotlib
-#import matplotlib.pyplot as pyplot
+#import matplotlib.pyplot as plot
 #This can be improved by calcualting drift on the fly but that is proabbly too difficult for me not to mention I don't know how to do that.
 
 #Not yet implemented offsetting the angle by 30 degrees for another drone.
-    
+nDrones = float(input("Number of drones :"))
+#areaWidth = float(input("What is the width of the area?"))
+#areaLength = float(input("What is the length of the area")) Both not currently needed
+
+driftSpeed = float(input("Drift speed of the target in ms^-1 :"))
+driftAngle = float(input("What is the drift angle of the target in degrees :"))#Do not say 0
+droneSpeed = float(input("What is the drone speed"))
+legTime = float(input("How long should each leg take"))
+
+print("Search Planner")
 #need to, using the speed of drift and direction of drift, 
 #You go first 2 on compass heading, then you come back towards the datum position after its drifted.
 def victorSierra(droneSpeed,driftSpeed,driftAngle,legTime): #Creates a path using sector search method used by the US and Canadian coastguards
     theta = [2*math.pi/6*i+driftAngle/180*math.pi for i in range (6)] #Calculates the angles of each sector 
-   #Finds the horizontal and vertical velocity for each leg.
+   #Finds the horizontal and verticle velocity for each leg.
     velocityX = [droneSpeed/legTime*math.cos(thetai) for (thetai) in theta]
     velocityY = [droneSpeed/legTime*math.sin(thetai) for (thetai) in theta]  
-    #Finds the horiziontal and vertical components of the drift speed after converting the drift angle to radians
-    driftX = driftSpeed/math.cos(driftAngle/180*math.pi)
-    driftY = driftSpeed/math.sin(driftAngle/180*math.pi)
+    #Finds the horiziontal and verticle components of the drift speed after converting the drift angle to radians
+    if math.cos(driftAngle/180*math.pi) == 0:
+        driftX = 0
+    else:
+        driftX = driftSpeed/math.cos(driftAngle/180*math.pi)
+    #if driftAngle=0 driftY = 0 Same of others !!!!, change to consider the trig value not the angle value.
+    if math.sin(driftAngle/180*math.pi) == 0:
+        driftY = 0
+    else:
+        driftY = driftSpeed/math.sin(driftAngle/180*math.pi)
     #Creates the forces for each direction
     forceX = [driftX*i for i in range (1,7)]
     forceY = [driftY*i for i in range (1,7)]
@@ -32,24 +48,5 @@ def victorSierra(droneSpeed,driftSpeed,driftAngle,legTime): #Creates a path usin
     for i in range(len(runPositionsX)):
         print (" [ %0.3f, %0.3f] " % (runPositionsX[i],runPositionsY[i]))
 
-    #pyplot.figure()
-    #pyplot.plot(runPositionsX,runPositionsY)
-
 #Calls victor Sierra, later this will be changed as we add more funcitons
-    
-moduleIsRunningStandalone = __name__ == "__main__"
-if moduleIsRunningStandalone:
-    nDrones = float(input("Number of drones :"))
-
-    #areaWidth = float(input("What is the width of the area?"))
-    #areaLength = float(input("What is the length of the area")) Both not currently needed
-
-    driftSpeed = float(input("Drift speed of the target in ms^-1 :"))
-    driftAngle = float(input("What is the drift angle of the target"))#Do not say 0
-    droneSpeed = float(input("What is the drone speed"))
-    legTime = float(input("How long should each leg take"))
-
-    print("Search Planner")
-
-    victorSierra(droneSpeed,driftSpeed,driftAngle,legTime)
-
+victorSierra(droneSpeed,driftSpeed,driftAngle,legTime)
